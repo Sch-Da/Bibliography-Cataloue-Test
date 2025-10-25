bf2b3fc1aeaf34e0e9998ffc18e454e8305bdf6c
Daniela Schneider <85058499+Sch-Da@users.noreply.github.com>
2025-10-25 17:57:45 +0200

    Update titles in textsInterface.json.js

--- Ch6.Rudolf.Geyer.Catalog/textsInterface.json.js
+++ Ch6.Rudolf.Geyer.Catalog/textsInterface.json.js
@@ -68,8 +68,8 @@ var texts =
         ,
         "beginTitleCatalog":
             {
-                "english": "All entries of the Geyer Catalogue",
-                "german": "Alle Einträge des Geyer-Katalogs"
+                "english": "All entries of the Catalogue",
+                "german": "Alle Einträge des Katalogs"
             }
         ,
         "noResultsTitleCatalog":
